f54b854312446c8f0ad0ed22c28589b56c6fa385
ablazquezh <alb.blazquez.h@gmail.com>
2025-03-29 15:07:59 +0100

    Fixed team stats view.

--- database_setup.py
+++ database_setup.py
@@ -279,7 +279,9 @@ creation_queries = ["CREATE TABLE teams (ID INT NOT NULL AUTO_INCREMENT, team_na
                                 WHEN pa.average >= (tb.team_avg - tb.team_std) 
                                 THEN pa.average 
                             END) AS position_avg_std,
-                        t.game
+                        t.game,
+                        t.team_league,
+                        t.team_country
                     FROM player_assignments pa
                     JOIN teams t ON pa.team_id_fk = t.id
                     LEFT JOIN leagues l ON pa.league_id_fk = l.id
